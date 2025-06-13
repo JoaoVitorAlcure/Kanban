40eacd7eb2c5c84f5017e78c94f0b04641c5b90e
João <joaovitoralcure@gmail.com>
2025-06-13 08:07:07 -0300

    Versão nova e com mais coisa

---
+++ app/src/main/java/com/example/kanban/ui/SplashFragment.kt
@@ -0,0 +1,26 @@
+package com.example.kanban.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kanban.R
+import com.example.kanban.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding:FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/kanban/ui/auth/RecoverAccountFragment.kt
@@ -0,0 +1,26 @@
+package com.example.kanban.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.kanban.R
+import com.example.kanban.databinding.FragmentRecoverAccountBinding
+
+class RecoverAccountFragment : Fragment() {
+
+    private var _binding: FragmentRecoverAccountBinding? = null
+    private val binding get() = _binding !!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
